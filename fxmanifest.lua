291103aa9de29419bd558d270df062cde590ea21
Roberto <78977643+LS-Robert@users.noreply.github.com>
2023-06-03 19:12:52 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,21 @@
-fx_version 'cerulean'
+--[[
+ _       _____        __   ______
+| |     / ____|      / /  |___  /
+| |    | (___       / /      / / 
+| |     \___ \     / /      / /  
+| |____ ____) |   / /      / /__ 
+|______|_____/   /_/      /_____|
 
+]]
+                                 
+                                 
+fx_version 'cerulean'
 game 'gta5'
 
-author "Anto Orza#6115"
-description "Z-Loading-Screen
-version '1.0.1'
+version '1.0.0'
+author 'Anto Orza#6115'
+description 'Z-Loading-Screen (https://orzafamily.com/)'
+repository 'https://github.com/LS-Robert/Z-Loading-Screen'
 
 loadscreen 'html/index.html'
 loadscreen_manual_shutdown 'yes'
